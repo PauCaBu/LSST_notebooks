539b7c494135096fef3fcb2ce722be3778c2acb2
PauCaBu <fantasilla@hotmail.com>
2022-10-27 12:08:24 -0300

    little changes, cant remember them asjasj

--- lsst_pyhelper.py
+++ lsst_pyhelper.py
@@ -945,21 +945,19 @@ def get_light_curve(repo, visits, collection_diff, collection_calexp, ccd_num, r
             #print(calib_path, ' doesnt exist')
             if i == 0:
                 calib = pc.DoCalibration(repo, visits_aux[0], ccd_num, collection_diff, config=config) 
-
-
-                calib_mean0 = calib[0]#calib_mean
-                calib_intercept0 = 0 #calib[1]#calib_intercept
+                calib_mean0 = calib[0] #calib_mean
+                calib_intercept0 = calib[1] #calib_intercept
             
             calib_rel = pc.DoRelativeCalibration(repo, visits_aux[0], calib_mean0, calib_intercept0, visits_aux[i], ccd_num, collection_diff, config=config) 
             calibRel_mean = calib_rel[0]
-            calibRel_intercept = 0 #calib_rel[1]
+            calibRel_intercept = calib_rel[1]
 
             calib_relative.append(calibRel_mean)
             calib_relative_intercept.append(calibRel_intercept)
         else:
             npzfile = np.load(calib_path)
             calib_relative = npzfile['x']
-            calib_relative_intercept= npzfile['y']
+            calib_relative_intercept= npzfile['y'] #np.zeros(len(calib_relative)) #
             calibRel_mean = calib_relative[i]
             calibRel_intercept = calib_relative_intercept[i]
 
